279306d499c1790ca5af80989379b9131b8c0626
Junior <web.juniorfreitas@gmail.com>
2017-07-06 19:05:44 -0300

    ordenando por coluna

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 
   <div id="app">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  
+
   	<table class='table table-striped'>
   		<thead>
 		  <tr>
@@ -12,7 +12,7 @@
 		    <td>Gols Marcados</td>
 		    <td>Gols Sofridos</td>
 		    <td>Saldo</td>
-		  </tr> 
+		  </tr>
 		 </thead>
 		 <tbody>
 		 	<tr v-for='time in times'>
@@ -25,5 +25,3 @@
   	<div id="app-3">
   	<p v-if="seen">Now you see me</p>
 	</div>
-
-
--- src/main.js
+++ src/main.js
@@ -1,19 +1,26 @@
-// The following line loads the standalone build of Vue instead of the runtime-only build,
+  // The following line loads the standalone build of Vue instead of the runtime-only build,
 // so you don't have to do: import Vue from 'vue/dist/vue'
 // This is done with the browser options. For the config, see package.json
 import Vue from 'vue'
 import App from './App.vue'
+import {Time} from './time'
+import _ from 'lodash';
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
   data: {
+      order:{
+        keys:['pontos', 'gm', 'gs'],
+        sort:['desc', 'desc', 'asc']
+      },
+      colunas:['nome','pontos','gm','gs','saldo'],
   	times:[
-  		{nome:'São paulo',gm: 0,gs:0, saldo:''},
-		{nome:'Corinthians',gm: 0,gs:0, saldo:''},
-		{nome:'Flamengo',gm: 0,gs:0, saldo:''},
-		{nome:'Gremio',gm: 0,gs:0, saldo:''},
-		{nome:'Inter',gm: 0,gs:0, saldo:''}
-  	],
+  		new Time('São paulo',''),
+        new Time('Corinthians',''),
+        new Time('Flamengo',''),
+        new Time('Gremio',''),
+        new Time('Inte','')
+  		],
   	novoJogo: {
 	    casa: {
 	        time: null,
@@ -23,21 +30,46 @@ new Vue({ // eslint-disable-line no-new
 	        time: null,
 	        gols: 0
 	    },
-	}
+	},
+  view: 'tabela'
   },
-  
-  created() {
-    let indexCasa = Math.floor(Math.random() * 5),
-         indexFora = Math.floor(Math.random() * 5);
+methods:{
+    fimJogo(){
+        let timeAdversario = this.novoJogo.fora.time;
+        let gols = +this.novoJogo.casa.gols; // o + converte pra inteiro
+        let golsAdversario = +this.novoJogo.fora.gols;
+        this.novoJogo.casa.time.fimJogo(timeAdversario,gols, golsAdversario);
+        this.view = 'tabela';
+    },
+    createNovoJogo(){
+        let indexCasa = Math.floor(Math.random() * 5),
+             indexFora = Math.floor(Math.random() * 5);
 
-    this.novoJogo.casa.time = this.times[indexCasa];
-    this.novoJogo.casa.gols = 0;
+        this.novoJogo.casa.time = this.times[indexCasa];
+        this.novoJogo.casa.gols = 0;
 
-    this.novoJogo.fora.time = this.times[indexFora];
-    this.novoJogo.fora.gols = 0;
-
+        this.novoJogo.fora.time = this.times[indexFora];
+        this.novoJogo.fora.gols = 0;
+        this.view = 'formNovoJogo';
+    },
+    sortByColum(colum){
+        this.order.keys = colum;
+        this.order.sort = this.order.sort == 'desc'? 'asc' : 'desc';
+    }
+},
+filters:{
+    saldo(time){
+        return time.gm - time.gs;
+    },
+    ucWords(value){
+        return value.charAt(0).toUpperCase() + value.slice(1);
+    },
+},
+computed:{
+        timesFiltered(){
+            return _.orderBy(this.times, this.order.keys, this.order.sort);
+        }
 }
-  	
+
 
 })
-
